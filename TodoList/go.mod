6d90e373fa05452f64e40422b40587e0c1d2184d
sertemo <tejedor.moreno@gmail.com>
2024-10-17 00:07:36 +0200

    tutorial Nana 2

---
+++ TodoList/go.mod
@@ -0,0 +1,3 @@
+module todolist-app
+
+go 1.23.2
---
+++ TodoList/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("##### Welcome to our TodoList app! #####")
+	fmt.Println("Task 1")
+	fmt.Println("Task 2")
+	fmt.Println("Task 3")
+}
