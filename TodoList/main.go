adabb6908cca843b2529075c7445844cd0d9e5e2
sertemo <tejedor.moreno@gmail.com>
2024-10-17 11:10:23 +0200

    terminando el tuto de nana

--- TodoList/main.go
+++ TodoList/main.go
@@ -6,7 +6,26 @@ import (
 
 func main() {
 	fmt.Println("##### Welcome to our TodoList app! #####")
-	fmt.Println("Task 1")
-	fmt.Println("Task 2")
-	fmt.Println("Task 3")
+
+	var shortGolang = "Watch Go crash course"
+	var fullGolang = "Watch Nana's full course"
+	var rewardDessert = "Reward myself with a pizza"
+	var taskItems = []string{shortGolang, fullGolang, rewardDessert}
+
+	printTasks(taskItems)
+	fmt.Println()
+	taskItems = addTask(taskItems, "Read Go book")
+	printTasks(taskItems)
+}
+
+func printTasks(tasks []string) {
+	fmt.Println("List of my ToDos:")
+	for i, task := range tasks {
+		fmt.Printf("%v: %v\n", i+1, task)
+	}
+}
+
+func addTask(taskItems []string, newTask string) []string {
+	updatedtaskItems := append(taskItems, newTask)
+	return updatedtaskItems
 }
