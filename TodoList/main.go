bb59701895ce064a42a1c4f1be9297afaec3737d
sertemo <tejedor.moreno@gmail.com>
2024-10-17 16:30:17 +0200

    terminado tutorial crash de Nana

--- TodoList/main.go
+++ TodoList/main.go
@@ -2,30 +2,64 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 )
+
+var taskItems []string
 
 func main() {
 	fmt.Println("##### Welcome to our TodoList app! #####")
 
-	var shortGolang = "Watch Go crash course"
-	var fullGolang = "Watch Nana's full course"
-	var rewardDessert = "Reward myself with a pizza"
-	var taskItems = []string{shortGolang, fullGolang, rewardDessert}
+	http.HandleFunc("/", greetUser)
+	http.HandleFunc("/show-tasks", showTasks)
+	http.HandleFunc("/add-task/", addTask)
 
-	printTasks(taskItems)
-	fmt.Println()
-	taskItems = addTask(taskItems, "Read Go book")
-	printTasks(taskItems)
+	http.ListenAndServe(":8080", nil)
+
 }
 
-func printTasks(tasks []string) {
-	fmt.Println("List of my ToDos:")
-	for i, task := range tasks {
-		fmt.Printf("%v: %v\n", i+1, task)
+func showTasks(writer http.ResponseWriter, request *http.Request) {
+	if len(taskItems) == 0 {
+		fmt.Fprintln(writer, "No hay tareas en la lista.")
+		return
+	}
+	for i, task := range taskItems {
+		fmt.Fprintf(writer, "%d: %s\n", i+1, task)
 	}
 }
 
-func addTask(taskItems []string, newTask string) []string {
-	updatedtaskItems := append(taskItems, newTask)
-	return updatedtaskItems
+func greetUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "Hello, user and Welcome. Enjoy the app"
+
+	fmt.Fprintln(writer, greeting)
+}
+
+func addTask(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		writer.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(writer, "Método no permitido. Por favor, usa una solicitud POST para agregar una tarea.", request.Method)
+		return
+	}
+
+	// Parsear el formulario
+	err := request.ParseForm()
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(writer, "Error al procesar el formulario.")
+		return
+	}
+
+	// Obtener la nueva tarea del formulario
+	newTask := request.FormValue("task")
+	newTask = strings.TrimSpace(newTask)
+	if newTask == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(writer, "La tarea no puede estar vacía.")
+		return
+	}
+
+	// Agregar la nueva tarea a la lista
+	taskItems = append(taskItems, newTask)
+	fmt.Fprintf(writer, "Tarea agregada: %s\n", newTask)
 }
